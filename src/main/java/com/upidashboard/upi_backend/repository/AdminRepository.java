84d7f1a8fd03170c0f1e370fafa33f57ddc1085b
Swarnim Singh <swarnimsingh921@gmail.com>
2025-11-09 21:43:20 +0530

    Fixed syntax error and merge issue in AdminRepository.java

--- src/main/java/com/upidashboard/upi_backend/repository/AdminRepository.java
+++ src/main/java/com/upidashboard/upi_backend/repository/AdminRepository.java
@@ -1,15 +1,8 @@
 package com.upidashboard.upi_backend.repository;
 
-import com.upidashboard.upi_backend.model.Admin;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
+import com.upidashboard.upi_backend.model.Admin;
 
-@Repository
 public interface AdminRepository extends MongoRepository<Admin, String> {
-<<<<<<< HEAD
+    // You can add custom query methods here later if needed
 }
-=======
-    // You can add custom queries later, for example:
-    Admin findByEmail(String email);
-}
->>>>>>> a9fde5b945b9b33778f4479bfb2c9c257e0e31fa
